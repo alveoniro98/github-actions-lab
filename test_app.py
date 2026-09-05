0b41b82caa3e9e07b7ddea6dae673b069a8dafb9
Alveoniro <alveonirorizaldiasmara@gmail.com>
2026-09-05 23:02:38 +0700

    Testing tests failure

--- test_app.py
+++ test_app.py
@@ -11,4 +11,10 @@ def test_add_negative():
     assert add(-2, 2) == 0
 
 def test_multiply():
-    assert multiply(2, 3) == 6
+    assert multiply(2, 3) == 7
+
+def test_multiply_zero():
+    assert multiply(10, 0) == 0
+
+def test_multiply_negative():
+    assert multiply(-2, 3) == -6
