9bb148292ad1063666f57a64d1a044fd9d1d8619
Alveoniro <alveonirorizaldiasmara@gmail.com>
2026-09-05 23:04:57 +0700

    Fix test

--- test_app.py
+++ test_app.py
@@ -11,7 +11,7 @@ def test_add_negative():
     assert add(-2, 2) == 0
 
 def test_multiply():
-    assert multiply(2, 3) == 7
+    assert multiply(2, 3) == 6
 
 def test_multiply_zero():
     assert multiply(10, 0) == 0
